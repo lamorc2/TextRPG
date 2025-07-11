1e09ff153db1f0b8d686019a0468868edf493e6b
ConnorLaMora <65458183+lamorc2@users.noreply.github.com>
2025-07-11 00:07:05 -0400

    Add files via upload

---
+++ game/item.cpp
@@ -0,0 +1,55 @@
+#include "item.h"
+
+
+	Item::Item(const ItemType& type_, const int& stat_value, const std::string& name_, const std::string& description_, bool stack, const std::string& ID){
+		itemID = ID;
+        type = type_;
+        name = name_;
+        stackable = stack;
+        description = description_;
+        switch(type_){
+
+        case ItemType::Drink:
+            healthRestore = stat_value;
+            break;
+
+        case ItemType::Doobie:
+            manaRestore = stat_value;
+            break;
+
+        case ItemType::Weapon:
+            damage = stat_value;
+            break;
+
+        case ItemType::Armor;
+            defense = stat_value;
+            break;
+
+        }
+    };
+
+    void Item::use(Player& player) const{
+        switch(type){
+
+            case ItemType::Drink:
+                //Drink code
+                player->restoreHealth(stat_value);
+                break;
+            case ItemType::Weapon:
+                //Weapon
+                player.equipArmor(this);
+                break;
+            case ItemType::Doobie:
+                //Doobie
+                break;
+            case ItemType::Armor:
+                //Armor
+                std::cout << "You equip the \033[33m" << name << "\033[0m!" << std::endl;
+                break;
+            default:
+                std::cerr << "Error 001 : Item Type not Recognized" << std::endl;
+                break;
+
+        }
+
+};
---
+++ game/player.h
@@ -0,0 +1,48 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+/*
+enum class PlayerType {
+    Rylan,
+    Cayden,
+    Caden, 
+    Connor,
+    Tim,
+    Dylan
+};
+*/
+
+class Player {
+public:
+
+    Player(const std::string& character);
+    std::vector<InventoryEntry> inventory;
+
+
+    //FIXME Should this be initialized as nullptr?
+    Item* equippedWeapon = nullptr;
+    Item* equippedArmor = nullptr;
+    Location* Location;
+    std::string name;
+    int health;
+    int max_health;
+    int mana;
+    int max_mana;
+    int lvl;
+    int strength; //base attack + weapon?? multiplier?
+    int armor_stat; //starting is 0, everytime armor is equipped, replace this value INSTEAD of accessing the armor data value for every damage
+    int damage_stat;//^^
+    // Class System? Specific boosts?
+
+    void level_up();
+    void restoreHealth(const int amnt);
+    void takeDamage(const int amnt);    
+    void equipArmor(InventoryEntry* armor);
+    void equipWeapon(InventoryEntry* weapon);
+
+   
+};
+
+#endif

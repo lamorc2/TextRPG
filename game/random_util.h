49d3d8da32456ab0c3e45256a4f7baa7e64bc22e
ConnorLaMora <65458183+lamorc2@users.noreply.github.com>
2025-07-17 22:23:41 -0400

    Delete game directory

--- game/random_util.h
+++
@@ -1,8 +0,0 @@
-namespace RandomUtils {
-    inline std::mt19937 gen(std::random_device{}());
-
-    inline double randomFloat(double min, double max) {
-        std::uniform_real_distribution<> dist(min, max);
-        return dist(gen);
-    }
-}
